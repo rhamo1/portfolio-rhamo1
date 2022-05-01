5ce54303ce92c3c2f1327772d76ecad3c5731a01
Rhamon Souza <rhamo1@outlook.com>
2022-04-30 22:56:49 -0300

    feat: p5js canvas.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,27 +1,50 @@
-import { Center, Image } from "@chakra-ui/react";
+import { Center, Image, Stack } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import useSound from "use-sound";
 import sound from "../../media/gospel.mp3";
-import lanssa from "../../media/cachorrada.mp3";
+// import lanssa from "../../media/cachorrada.mp3";
+import img1 from "../../media/pics/red.png";
+import img2 from "../../media/pics/globe.png";
+import { Canvas } from "../../components/Canvas";
 
 export const Home = () => {
   const [play] = useSound(sound);
   const history = useHistory();
   return (
-    <Center flexDirection={"column"} height="100vh" alignItems={"center"}>
-      <h3>gospeldownloads</h3>
-      <Button variant={"ButtonBlock"} onClick={() => history.push("/fodase")}>
-        aaaaaaaaa
+    <Center
+      border={"14px"}
+      borderColor="black"
+      flexDirection={"column"}
+      height="100%"
+      alignItems={"center"}
+    >
+      <Button
+        padding={"10px"}
+        variant={"BlockButton"}
+        onClick={() => history.push("/fodase")}
+      >
+        margin={"20px"}
       </Button>
-      <p>adasdasdas</p>
+      <p>text</p>
       <Image
+        margin={"20px"}
         maxWidth={"70%"}
         src="https://static.corinthians.com.br/uploads/1639404326db095bd14b838cbf3abf886f492c721b.jpg"
       />
-      <Button onClick={() => play(sound)}>gospel</Button>
-      {/* <Button onClick={() => play(lanssa)}>cachorrada</Button> */}
+      <Stack>
+        <Button padding={"15px"} onClick={() => play(sound)}>
+          gospel
+        </Button>
+        <Button padding={"15px"} onClick={() => history.push("/canvas")}>
+          canvas
+        </Button>
+      </Stack>
+      {/* <Canvas /> */}
+      <h1>® GOSPELDOWNLOADS 2022 ®</h1>
+      <Image margin={"20px"} maxWidth={"70%"} src={img1} />
+      <h1>/</h1>
+      <Image margin={"20px"} maxWidth={"70%"} src={img2} />
     </Center>
   );
 };
--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -1,23 +1,22 @@
-import DOMPurify from "dompurify";
+import React from "react";
+import Sketch from "react-p5";
 
-const rawHTML = `
-<!DOCTYPE html><html lang="en"><head>
-    <script src="p5.js"></script>
-    <script src="p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8">
-
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-    <p>teste</p>
-  
-
-</body></html>
-`;
+let x = 50;
+let y = 50;
 
 export const Canvas = () => {
-  return <div dangerouslySetInnerHTML={{ __html: rawHTML }}></div>;
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(900, 900).parent(canvasParentRef);
+  };
+  const draw = (p5) => {
+    if (p5.mouseIsPressed) {
+      p5.fill(250);
+    } else {
+      p5.fill(255);
+    }
+    p5.ellipse(p5.mouseX, p5.mouseY, 100, 80);
+  };
+  return <Sketch setup={setup} draw={draw} />;
 };
