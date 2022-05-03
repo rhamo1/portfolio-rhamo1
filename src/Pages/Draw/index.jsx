6538f1f5f3bce361702ec3a392ce735df23afc93
Rhamon Souza <rhamo1@outlook.com>
2022-05-03 20:56:06 -0300

    feat: mobile queries.

--- src/Pages/Draw/index.jsx
+++ src/Pages/Draw/index.jsx
@@ -4,13 +4,43 @@ import React from "react";
 import Sketch from "react-p5";
 import { useHistory } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  console.log(height, width);
+
+  return {
+    width,
+    height,
+  };
+};
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
 export const Draw = () => {
   const history = useHistory();
-  console.log(typeof h);
+  const { height, width } = useWindowDimensions();
+
   const setup = (p5, canvasParentRef) => {
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
-    p5.createCanvas(1000, 900).parent(canvasParentRef);
+    p5.createCanvas(width - 100, height - 100).parent(canvasParentRef);
   };
 
   let colorValue = 250;
@@ -32,7 +62,7 @@ export const Draw = () => {
       colorValue2 = p5.mouseY * 0.09;
 
       p5.fill(colorValue, colorValue1, colorValue2);
-      p5.circle(p5.mouseY, p5.mouseX, sizey / 2, sizex / 2);
+      p5.circle(p5.mouseY, p5.mouseX, sizey / 2);
     } else {
       p5.fill(colorValue, colorValue1, colorValue2);
       p5.rect(p5.mouseY, p5.mouseX, sizex / 2, sizey / 2);
